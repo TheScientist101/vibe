d98ec1999a03aca0e547b131b41d083b11eadb44
Urjith Mishra <66270347+TheScientist101@users.noreply.github.com>
2025-05-07 21:10:51 -0500

    Remove expo stuff

---
+++ vibe/vibeApp.swift
@@ -0,0 +1,17 @@
+//
+//  vibeApp.swift
+//  vibe
+//
+//  Created by Urjith Mishra on 5/7/25.
+//
+
+import SwiftUI
+
+@main
+struct vibeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
