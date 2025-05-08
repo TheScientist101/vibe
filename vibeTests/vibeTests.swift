4cefb757019d0bbabe1ff196c39340a660ac8a04
Urjith Mishra <66270347+TheScientist101@users.noreply.github.com>
2025-05-07 21:10:51 -0500

    Remove expo stuff

---
+++ vibeTests/vibeTests.swift
@@ -0,0 +1,17 @@
+//
+//  vibeTests.swift
+//  vibeTests
+//
+//  Created by Urjith Mishra on 5/7/25.
+//
+
+import Testing
+@testable import vibe
+
+struct vibeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
