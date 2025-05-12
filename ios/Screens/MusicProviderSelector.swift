3f8bfe1146da8691ce03d23225705e0bc1f83e10
Jayden Chun <jayden.h.chun@gmail.com>
2025-05-12 13:29:44 -0500

    added music screen

--- ios/Screens/MusicProviderSelector.swift
+++ ios/Screens/MusicProviderSelector.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+import SwiftUI
+struct MusicProviderSelector : View {
+    @Published var musicProviderSpotify = false;
+    var body: some View{
+        VStack{
+            Text("Select Your Music Provider:")
+            HStack{
+                Button(action: {musicProviderSpotify = true; print(musicProviderSpotify)
+                    }){
+                    Image("SpotifyLogo")
+                        .resizable()
+                        .frame(width: 100, height: 100)
+                }
+                Button(action: {musicProviderSpotify = false;
+                    
+                    print(musicProviderSpotify);
+                    }) {
+                    Image("AppleMusicLogo")
+                        .resizable()
+                        .frame(width: 100, height: 100)
+                }
+            }
+        }
+    }
+    
+    
+}
+#Preview{
+    MusicProviderSelector()
+}
