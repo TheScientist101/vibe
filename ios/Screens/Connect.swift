e9b49cfd3cb7e8f0334fde36add990e439e4eaf4
Jayden Chun <jayden.h.chun@gmail.com>
2025-07-04 02:05:58 -0500

    added firebse db dependencies + added the capability of creating a vibe session through the db

--- ios/Screens/Connect.swift
+++ ios/Screens/Connect.swift
@@ -6,10 +6,48 @@
 //
 
 import SwiftUI
+import FirebaseCore
+import FirebaseFirestore
 
 struct Connect: View {
+    let db = Firestore.firestore()
     var body: some View {
-        Text("Connect to others!")
+        GeometryReader{ geometry in
+            VStack{
+                Button(
+                    action:{print("connecting fr trust there's something happening in the backend")}
+                ){
+                    Text("Join a Vibe Session")
+                        .bold()
+                }
+                .background(Color.orange)
+                .border(Color.white, width: 1)
+                Button(
+                    action:{
+                        print("creating a vibe session")
+                        createVibeSession()
+                    }
+                ){
+                    Text("Create a Vibe Session")
+                        .bold()
+                }
+                .background(Color.orange)
+                .border(Color.white, width: 1)
+            }
+            .position(x: geometry.size.width*0.5, y: geometry.size.height*0.5)
+        }
+    }
+    private func createVibeSession() {
+        Task{
+            do{
+                let ref = try await db.collection("vibeSessions").addDocument(data: [
+                    "sessionID": UUID().uuidString
+                ])
+                print("Document added with ID: \(ref.documentID)")
+            } catch{
+                print("Error adding document: \(error)")
+            }
+        }
     }
 }
 
--- ios/Screens/SongListViewSpotify.swift
+++ ios/Screens/SongListViewSpotify.swift
@@ -16,39 +16,49 @@ struct SongListViewSpotify: View {
     var body: some View {
         ScrollView {
             ForEach(songs) { SpotifyTrack in
-                Button(action: {
-                    onSelect(SpotifyTrack)
-                }) {
-                    HStack(){
-                        AsyncImage(url: URL(string: SpotifyTrack.album.imageUrl ?? "")) { image in
-                            image
-                                .resizable()
-                                .scaledToFill()
-                        } placeholder: {
-                            ProgressView()
+                HStack{
+                    Button(action: {
+                        onSelect(SpotifyTrack)
+                    }) {
+                        HStack(){
+                            AsyncImage(url: URL(string: SpotifyTrack.album.imageUrl ?? "")) { image in
+                                image
+                                    .resizable()
+                                    .scaledToFill()
+                            } placeholder: {
+                                ProgressView()
+                            }
+                                .frame(width: 64, height: 64)
+                                .cornerRadius(8)
+                                .padding([.vertical, .trailing], 8)
+                            VStack{
+                                Text(SpotifyTrack.name)
+                                    .font(.headline)
+                                    .foregroundColor(Color.white)
+                                    .bold(true)
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                    .lineLimit(1)
+                                Text(SpotifyTrack.artistName)
+                                    .font(.subheadline)
+                                    .foregroundColor(Color.gray)
+                                    .bold(true)
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                    .lineLimit(1)
+                            }
+                                Image(systemName: "play.fill")
+                                    .font(.system(size: 24))
                         }
-                            .frame(width: 64, height: 64)
-                            .cornerRadius(8)
-                            .padding([.vertical, .trailing], 8)
-                        VStack{
-                            Text(SpotifyTrack.name)
-                                .font(.headline)
-                                .foregroundColor(Color.white)
-                                .bold(true)
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                                .lineLimit(1)
-                            Text(SpotifyTrack.artistName)
-                                .font(.subheadline)
-                                .foregroundColor(Color.gray)
-                                .bold(true)
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                                .lineLimit(1)
-                        }
-                            Image(systemName: "play.fill")
-                                .font(.system(size: 24))
+                        .frame(maxWidth: .infinity, alignment: .leading)
                     }
-                    .frame(maxWidth: .infinity, alignment: .leading)
                 }
+                Button(
+                    action:{print("add to queue")}
+                )
+                {
+                    Image(systemName: "add.circle.fill")
+                        .font(.system(size: 24))
+                }
+                
             }
         }
         
